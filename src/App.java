bcd31078470144ca06611c32aa18f34cf47275c4
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-12-13 10:02:40 +0100

    finish - gjorde klart uppg1 del A och B

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //A
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.print("Användarnamn: ");
+        String namn = tangentbord.nextLine();
+        System.out.print("Lösenord: ");
+        String lösenord = tangentbord.nextLine();
+        if (namn.equals("root") && lösenord.equals("pass")) {
+            System.out.println("Ditt bankkonto har 0 SEK.");
+        }else{
+            System.out.println("Fel namn eller lösenord.");
+        }
+
+        //B
+        int tal = tangentbord.nextInt();
+        tangentbord.nextLine();
+        if (tal==10) {
+            System.out.println("Talet är lika med eller större än 10");
+        }else{
+            System.out.println("Talet är lika med eller mindre än 10");
+        }
+        tangentbord.close();
     }
 }
